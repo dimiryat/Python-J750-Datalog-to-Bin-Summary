b758a2036be2d8eaf519bfd1326ac622b733cf9c
Dennis Lin <dennis.dlin2@gmail.com>
2016-12-27 16:25:22 +0800

    New feature: Read multiple file from current folder
    New feature: Read multiple file from current folder except Python related file
    i.e. The program itself

--- J750DatalogBinSummary.py
+++ J750DatalogBinSummary.py
@@ -1,8 +1,11 @@
 #J750DatalogBinSummary.py
 #Creator: Dennis Lin
 
+import re
+from glob import glob
 import pandas
 import numpy
+import fileinput
 
 GetEnable=False
 ResultString=[]
@@ -10,13 +13,19 @@ ResutlDict={'Site':[],
             'Soft Bin':[],
             'Hard Bin':[]}
 
-SourceFile="C:\\Users\dlin\Downloads\J750-59_FT1RT1_V2A0_82E1_E4.9.0_PIXEL_110416_N0BY16.00B_N0BY16.00_Nov29_2300_16.data"
-Source=open(SourceFile,"r")
-data=Source.read()
-Source.close()
+#This block is for reading all the files' name from current folder
+#Then read all files' content to a string variable "Content" except Python related files
+Content=""
+fnames=glob("*.*")
+for s in fnames:
+    if re.search("\.*py",s)==None:
+        Source=open(s,"r")
+        Content=Content+Source.read()
+        Source.close()
 DataSplit=[]
-DataSplit=data.split("\n")
+DataSplit=Content.split("\n")
 
+#Do some compare to append the string it needs from the files
 for TempStr in DataSplit:
     if (TempStr==" Site    Sort     Bin"):
         GetEnable=True
@@ -28,25 +37,17 @@ for TempStr in DataSplit:
     if (TempStr=="========================================================================="):
         GetEnable=False
 
+#Append the information we got into dictionary variable "ResultDict"
 for Temp in ResultString:
     temp2=Temp.split()
     ResutlDict['Site'].append(int(temp2[0]))
     ResutlDict['Soft Bin'].append(int(temp2[1]))
     ResutlDict['Hard Bin'].append(int(temp2[2]))
 
+#Convert "ResultDict" to DataFrame
+#Then calculate PV table and convert them into csv file
 ResultData=pandas.DataFrame(ResutlDict)
 ResultData.to_csv('RawData.csv')
 ResultDataPV=pandas.pivot_table(ResultData,index='Soft Bin',columns='Site',aggfunc='count',fill_value=0,margins=True)
 print(ResultDataPV)
 ResultDataPV.to_csv('PV.csv')
-
-del ResultString
-del data
-del Temp
-del temp2
-del ResultData
-del DataSplit
-del TempStr
-del GetEnable
-del pandas
-del numpy
